1662c9eccd950dd2c62eb3ec2089e7abae599ab0
powerwmj <105851917+sleay@users.noreply.github.com>
2022-07-25 13:15:26 +0900

    TEST

--- js/allcata.js
+++ js/allcata.js
@@ -1,8 +1,13 @@
+let a = new Date();
+let day=a.getDay();
+
+
+
 
 
 $.ajax({
     
-    url:`https://korea-webtoon-api.herokuapp.com/all/week?day=0`,
+    url:`https://korea-webtoon-api.herokuapp.com/all/week?day=${day}`,
     beforeSend:function(){
         console.log('bbb')
     },
@@ -47,3 +52,102 @@ $.ajax({
         })
     }
 })
+
+
+
+
+
+
+
+
+
+key = 0;
+$('.nav > li').each(function(k1,v1){
+
+    if(day==k1){
+        $(v1).addClass('active')
+    }
+
+
+    $(v1).on('click',function(){
+        key = k1;
+        console.log(key)
+
+        
+        $('.nav > li').removeClass('active')
+        $('.nav > li').eq(k1).addClass('active')
+
+
+
+
+
+
+
+        $.ajax({
+    
+            url:`https://korea-webtoon-api.herokuapp.com/all/week?day=${key}`,
+            success:function(json){
+                let tag='';
+                let tag1='';
+                let tag2='';
+                let tag3='';
+                data=json;
+                console.log('ok')
+                $.each(data,function(k,v){
+            
+                    
+
+                        
+                    if(k < 6){
+                    tag +=`<li>
+                        <img src="${v.img}" alt="">
+                        <p>${v.title}</p>
+                    </li>`;
+                            $(".list1").html(tag)
+                            
+                            
+                    }
+                    if(k > 6 &&k < 13){
+                        tag1 +=`<li>
+                        <img src="${v.img}" alt="">
+                        <p>${v.title}</p>
+                    </li>`;
+                            $(".list2").html(tag1)
+                    }
+                    if(k > 13 &&k < 20){
+                        tag2 +=`<li>
+                        <img src="${v.img}" alt="">
+                        <p>${v.title}</p>
+                    </li>`;
+                            $(".list3").html(tag2)
+                    }
+                    if(k > 20 &&k < 27){
+                        tag3 +=`<li>
+                        <img src="${v.img}" alt="">
+                        <p>${v.title}</p>
+                    </li>`;
+                            $(".list4").html(tag3)
+                    }
+                }
+                )
+            }
+        })
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$('header').load('inc.html header > div');
+$('footer').load('inc.html footer > div');
