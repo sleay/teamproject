5f585f18889b4d980535e1a8c4ec380f7d7474dc
양영훈 <105851917+sleay@users.noreply.github.com>
2024-09-10 11:31:57 +0900

    Update finished.js
    -cc7dda2f0d77 변경

--- js/finished.js
+++ js/finished.js
@@ -9,7 +9,7 @@ $('footer').load('inc.html footer > div');
 
 $.ajax({
     
-    url:`https://korea-webtoon-api.herokuapp.com/all/finished`,
+    url:`https://korea-webtoon-api-cc7dda2f0d77.herokuapp.com/all/finished`,
     beforeSend:function(){
         console.log('bbb')
     },
@@ -90,7 +90,7 @@ $('.nav > li').each(function(k1,v1){
         if(k1==0){
             $.ajax({
     
-                url:`https://korea-webtoon-api.herokuapp.com/all/finished`,
+                url:`https://korea-webtoon-api-cc7dda2f0d77.herokuapp.com/all/finished`,
                 success:function(json){
                     let tag='';
                     let tag1='';
@@ -150,7 +150,7 @@ $('.nav > li').each(function(k1,v1){
 
         $.ajax({
     
-            url:`https://korea-webtoon-api.herokuapp.com/all/finished`,
+            url:`https://korea-webtoon-api-cc7dda2f0d77.herokuapp.com/all/finished`,
             success:function(json){
                 let tag='';
                 let tag1='';
