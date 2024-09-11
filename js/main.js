5aae7c01e22b408cac376bc447536a1bbe85a89a
양영훈 <105851917+sleay@users.noreply.github.com>
2024-09-11 17:07:36 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -29,7 +29,7 @@ setInterval(function(){
 
 $.ajax({
     
-    url:`https://korea-webtoon-api-cc7dda2f0d77.herokuapp.com/all/updateDay?day=${day}`,
+    url:`https://korea-webtoon-api-cc7dda2f0d77.herokuapp.com/webtoons?updateDay=MON`,
     beforeSend:function(){
         console.log('bbb')
     },
